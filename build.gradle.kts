ca530823c979633479efd2a348682f3f4344fc3a
Hunter Wignall <hwignall2000@gmail.com>
2021-09-18 21:25:27 -0400

    Functionality for building parameter detail v1.3.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "me.hwiggy"
-version = "1.3.0"
+version = "1.3.1"
 
 repositories {
     mavenLocal()
--- src/main/kotlin/me/hwiggy/kommander/arguments/Synopsis.kt
+++ src/main/kotlin/me/hwiggy/kommander/arguments/Synopsis.kt
@@ -8,18 +8,11 @@ import me.hwiggy.kommander.InvalidParameterException
  *
  */
 class Synopsis(init: Configurator.() -> Unit) {
-    private val elements = ArrayList<Pair<ElementType, String?>>()
     private val parameters = ArrayList<Parameter<*>>()
-
-    init {
-        Configurator().also(init)
-    }
-
-    /**
-     * Adds a Synopsis Element to the element list
-     */
-    private fun addElement(type: ElementType, element: Any?) {
-        elements += type to element?.toString()
+    private val elements = ArrayList<ElementGroup>()
+    private class ElementGroup : ArrayList<Pair<ElementType, String>>() {
+        fun add(type: ElementType, element: String) = add(type to element)
+        fun add(type: ElementType, element: Any) = add(type, element.toString())
     }
 
     /**
@@ -27,28 +20,116 @@ class Synopsis(init: Configurator.() -> Unit) {
      * Responsible for adding parameter-specific elements to the element list
      */
     private fun addParameter(parameter: Parameter<*>) = parameter.apply {
-        addElement(ElementType.PARAMETER_NAME, name)
-        addElement(ElementType.PARAMETER_DESCRIPTION, description)
-        if (this.default != null) addElement(ElementType.PARAMETER_DEFAULT, default)
+        val elementGroup = ElementGroup()
+        elementGroup.add(ElementType.PARAMETER_NAME, name)
+        elementGroup.add(ElementType.PARAMETER_DESCRIPTION, description)
+        if (this.default != null) elementGroup.add(ElementType.PARAMETER_DEFAULT, default!!)
         (adapter as? BoundAdapter<*>)?.apply {
-            if (min != null) addElement(ElementType.VALUE_MIN, min)
-            if (max != null) addElement(ElementType.VALUE_MAX, max)
+            if (min != null) elementGroup.add(ElementType.VALUE_MIN, min)
+            if (max != null) elementGroup.add(ElementType.VALUE_MAX, max)
         }
-    }.also(parameters::add)
+        elements.add(elementGroup)
+    }.run(parameters::add)
 
     /**
      * Adds a [Group] to the parameters list
      * Responsible for adding group-specific elements to the element list
      */
     private fun addGroup(group: Group<*>) = group.apply {
-        addElement(ElementType.GROUP_NAME, name)
-        addElement(ElementType.GROUP_DESCRIPTION, description)
+        val elementGroup = ElementGroup()
+        elementGroup.add(ElementType.GROUP_NAME, name)
+        elementGroup.add(ElementType.GROUP_DESCRIPTION, description)
         choices.forEach { (option, description) ->
-            addElement(ElementType.GROUP_OPTION, option.synopsisName)
-            addElement(ElementType.GROUP_OPTION_DESCRIPTION, description)
+            elementGroup.add(ElementType.GROUP_OPTION, option.synopsisName)
+            elementGroup.add(ElementType.GROUP_OPTION_DESCRIPTION, description)
         }
-        if (this.default != null) elements += ElementType.GROUP_DEFAULT to default!!.synopsisName
+        if (this.default != null) elementGroup.add(ElementType.GROUP_DEFAULT, default!!.synopsisName)
+        elements.add(elementGroup)
     }.also(parameters::add)
+
+    /**
+     * Processes arguments using the defined parameters list
+     */
+    fun process(input: Arguments) = parameters.associateTo(HashMap()) {
+        val out = input.next(it.adapter) ?: it.default
+        if (it is Group<*> && !it.isValid(out)) throw InvalidParameterException(out, it.name)
+        it.name to out
+    }.let(input::Processed)
+
+    /**
+     * Joins the parameter list to a string, for syntax
+     */
+    fun concatParameters() = if (parameters.isEmpty()) null else parameters.joinToString(" ") {
+        val identifier = if (it is Group<*>) it.concatOptions() else it.name
+        return@joinToString if (it.isOptional()) "($identifier)" else "<$identifier>"
+    }
+
+    /**
+     * Builds the parameter detail, taking into account the type of elements and their grouping.
+     * @param[elementModifier] The modifier for rendering an element based on its type
+     */
+    fun buildParameterDetail(
+        elementModifier: (ElementType, String) -> String,
+        elementJoiner: (Collection<String>) -> String = { it.joinToString() },
+        groupJoiner: (Collection<String>) -> String = { it.joinToString() }
+    ) = elements.map { group ->
+        group.map { elementModifier(it.first, it.second) }.let(elementJoiner)
+    }.let(groupJoiner)
+
+    /**
+     * Marker enum for Synopsis Elements
+     */
+    enum class ElementType {
+        /**
+         * The name of a parameter
+         */
+        PARAMETER_NAME,
+
+        /**
+         * The description for a parameter
+         */
+        PARAMETER_DESCRIPTION,
+
+        /**
+         * The default option for a parameter
+         */
+        PARAMETER_DEFAULT,
+
+        /**
+         * The name of a group
+         */
+        GROUP_NAME,
+
+        /**
+         * The description for a group
+         */
+        GROUP_DESCRIPTION,
+
+        /**
+         * An option for a group
+         */
+        GROUP_OPTION,
+
+        /**
+         * The description for an option for a group
+         */
+        GROUP_OPTION_DESCRIPTION,
+
+        /**
+         * The default option for a group
+         */
+        GROUP_DEFAULT,
+
+        /**
+         * The minimum value for a parameter
+         */
+        VALUE_MIN,
+
+        /**
+         * The maximum value for a parameter
+         */
+        VALUE_MAX
+    }
 
     inner class Configurator {
         /**
@@ -131,84 +212,13 @@ class Synopsis(init: Configurator.() -> Unit) {
             override fun isOptional() = true
         }.run(::addGroup)
     }
-
-    /**
-     * Processes arguments using the defined parameters list
-     */
-    fun process(input: Arguments) = parameters.associateTo(HashMap()) {
-        val out = input.next(it.adapter) ?: it.default
-        if (it is Group<*> && !it.isValid(out)) throw InvalidParameterException(out, it.name)
-        it.name to out
-    }.let(input::Processed)
-
-    /**
-     * Joins the parameter list to a string, for syntax
-     */
-    fun concatParameters() = if (parameters.isEmpty()) null else parameters.joinToString(" ") {
-        val identifier = if (it is Group<*>) it.concatOptions() else it.name
-        return@joinToString if (it.isOptional()) "($identifier)" else "<$identifier>"
-    }
-
-    /**
-     * Marker enum for Synopsis Elements
-     */
-    enum class ElementType {
-        /**
-         * The name of a parameter
-         */
-        PARAMETER_NAME,
-
-        /**
-         * The description for a parameter
-         */
-        PARAMETER_DESCRIPTION,
-
-        /**
-         * The default option for a parameter
-         */
-        PARAMETER_DEFAULT,
-
-        /**
-         * The name of a group
-         */
-        GROUP_NAME,
-
-        /**
-         * The description for a group
-         */
-        GROUP_DESCRIPTION,
-
-        /**
-         * An option for a group
-         */
-        GROUP_OPTION,
-
-        /**
-         * The description for an option for a group
-         */
-        GROUP_OPTION_DESCRIPTION,
-
-        /**
-         * The default option for a group
-         */
-        GROUP_DEFAULT,
-
-        /**
-         * The minimum value for a parameter
-         */
-        VALUE_MIN,
-
-        /**
-         * The maximum value for a parameter
-         */
-        VALUE_MAX
-    }
+    init { Configurator().also(init) }
 }
 
 /**
  * Marker interface for a Synopsis parameter
  */
-interface Parameter<T> {
+private interface Parameter<T> {
     /**
      * The name of this parameter
      */
