c3597fb1a2c94eca4412337424c1566eca1f86ba
Hunter Wignall <hwignall2000@gmail.com>
2021-11-24 19:48:20 -0500

    Update Kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.5.21"
+    kotlin("jvm") version "1.6.0"
     id("maven-publish")
 }
 
@@ -12,7 +12,7 @@ repositories {
 }
 
 dependencies {
-    compileOnly(kotlin("stdlib"))
+    compileOnly("org.jetbrains.kotlin:kotlin-stdlib:1.6.0")
 }
 
 publishing {
